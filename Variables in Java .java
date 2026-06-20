922fc1590cd639f4abf6f08db2c3caaf1872183b
Tabassum Ridy <tabassumridy461@gmail.com>
2026-06-21 00:18:09 +0600

    Create Variables in Java .java

---
+++ Variables in Java .java
@@ -0,0 +1,32 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        // String variable
+        String name = "John";
+        System.out.println(name);
+
+        // int variable
+        int myNum = 15;
+        System.out.println(myNum);
+
+        // Declare first, assign later
+        int myNumber;
+        myNumber = 15;
+        System.out.println(myNumber);
+
+        // Change variable value
+        int num = 15;
+        num = 20;
+        System.out.println(num);
+
+        // Other common variable types
+        double price = 99.99;
+        System.out.println(price);
+
+        char grade = 'A';
+        System.out.println(grade);
+
+        boolean isJavaFun = true;
+        System.out.println(isJavaFun);
+    }
+}
