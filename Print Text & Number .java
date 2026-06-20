3d8b21d9bac405e0f0b03e308243fe03ee4727e6
Tabassum Ridy <tabassumridy461@gmail.com>
2026-06-21 00:48:19 +0600

    Create Print Text & Number .java

---
+++ Print Text & Number .java
@@ -0,0 +1,9 @@
+public class PrintTextAndNumber {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 6;
+
+        System.out.println("The sum is " + x + y);   // Prints: The sum is 56
+        System.out.println("The sum is " + (x + y)); // Prints: The sum is 11
+    }
+}
