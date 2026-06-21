f29b224baa4becaf8a0ef4f54fe3ff02147eaf77
Tabassum Ridy <tabassumridy461@gmail.com>
2026-06-21 22:14:25 +0600

    Create Java MyMethod .java

---
+++ Java MyMethod .java
@@ -0,0 +1,15 @@
+public class MyMethod {
+
+    // Method
+    static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+
+    public static void main(String[] args) {
+
+        // Calling the method multiple times
+        myMethod();
+        myMethod();
+        myMethod();
+    }
+}
