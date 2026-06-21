87aa92eed451e0d8ed779a192ea357ab41590f26
Tabassum Ridy <tabassumridy461@gmail.com>
2026-06-21 21:29:59 +0600

    Create Characters .java

---
+++ Characters .java
@@ -0,0 +1,18 @@
+public class Characters {
+    public static void main(String[] args) {
+
+        // Character
+        char myGrade = 'B';
+        System.out.println("Grade: " + myGrade);
+
+        // ASCII Values
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println("ASCII 65: " + myVar1);
+        System.out.println("ASCII 66: " + myVar2);
+        System.out.println("ASCII 67: " + myVar3);
+
+        // String
+        String greeting = "Hello World";
+        System.out.println("Greeting: " + greeting);
+    }
+}
