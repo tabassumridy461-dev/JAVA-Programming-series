c20e55fa820ae54e10efe8e1fa7760634ca1f1e4
Tabassum Ridy <tabassumridy461@gmail.com>
2026-06-21 21:41:00 +0600

    Create Use of Var Keyword.java

---
+++ Use of Var Keyword.java
@@ -0,0 +1,12 @@
+public class VarKeyword {
+    public static void main(String[] args) {
+
+        var x = 5;           // int
+        var name = "Java";   // String
+        var price = 9.99;    // double
+
+        System.out.println("x = " + x);
+        System.out.println("name = " + name);
+        System.out.println("price = " + price);
+    }
+}
