37afb9414e07c87e71713005571bc2612275aefe
Tabassum Ridy <tabassumridy461@gmail.com>
2026-06-21 21:22:39 +0600

    Create NumbersInJava .java

---
+++ NumbersInJava .java
@@ -0,0 +1,14 @@
+public class Numbers {
+    public static void main(String[] args) {
+
+        byte myByte = 100;
+        short myShort = 5000;
+        int myInt = 100000;
+        long myLong = 15000000000L;
+
+        System.out.println("Byte: " + myByte);
+        System.out.println("Short: " + myShort);
+        System.out.println("Int: " + myInt);
+        System.out.println("Long: " + myLong);
+    }
+}
