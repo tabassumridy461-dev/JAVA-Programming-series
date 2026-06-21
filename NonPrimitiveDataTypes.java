fc346be898c3020c22ac808a620e4e294dc1fc71
Tabassum Ridy <tabassumridy461@gmail.com>
2026-06-21 21:35:13 +0600

    Create NonPrimitiveDataTypes.java

---
+++ NonPrimitiveDataTypes.java
@@ -0,0 +1,14 @@
+public class NonPrimitiveDataTypes {
+    public static void main(String[] args) {
+
+        // String
+        String name = "Tabassum";
+
+        // Array
+        int[] numbers = {10, 20, 30};
+
+        // Print values
+        System.out.println("Name: " + name);
+        System.out.println("First Number: " + numbers[0]);
+    }
+}
