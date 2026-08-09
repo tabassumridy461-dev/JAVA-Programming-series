457bb3c0509629e15d4a246e31cf9fa0a8585626
Tabassum Ridy <tabassumridy461@gmail.com>
2026-08-09 23:09:01 +0600

    Delete Print Text in Java.java

--- Print Text in Java.java
+++
@@ -1,7 +0,0 @@
-public class PrintText {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("I am learning Java.");
-        System.out.println("It is awesome!");
-    }
-}
